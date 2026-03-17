c25dabebdfa8b49871eb7e0a97030e8338bf51e9
Sho <sho@device-174.home>
2026-03-17 14:26:18 +0100

    Force dynamic homepage content

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { readHuntContent } from "@/lib/content-store";
 import { SiteLockGate } from "@/components/site-lock-gate";
 import { TreasureHuntApp } from "@/components/treasure-hunt-app";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const { huntConfig, treasureSteps } = await readHuntContent();
 
